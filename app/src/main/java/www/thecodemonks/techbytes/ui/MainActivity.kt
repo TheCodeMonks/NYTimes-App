2b8fde2dcecadcb9354eb54121da3a0deeb3fc2c
Sanju S <spikeysanju98@gmail.com>
2020-07-19 15:17:42 +0530

    ✅ Moved rss parser function to ViewModel
    Signed-off-by: Sanju S <spikeysanju98@gmail.com>

--- app/src/main/java/www/thecodemonks/techbytes/ui/MainActivity.kt
+++ app/src/main/java/www/thecodemonks/techbytes/ui/MainActivity.kt
@@ -11,15 +11,10 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import org.jsoup.Jsoup
 import www.thecodemonks.techbytes.R
 import www.thecodemonks.techbytes.adapter.CategoryAdapter
 import www.thecodemonks.techbytes.adapter.NewsAdapter
 import www.thecodemonks.techbytes.db.ArticleDatabase
-import www.thecodemonks.techbytes.model.Article
 import www.thecodemonks.techbytes.model.Category
 import www.thecodemonks.techbytes.repo.Repo
 import www.thecodemonks.techbytes.utils.Utils
@@ -29,6 +24,9 @@ import www.thecodemonks.techbytes.viewmodel.NewsViewModelProviderFactory
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: NewsViewModel
+    private lateinit var newsAdapter: NewsAdapter
+    private lateinit var categoryAdapter: CategoryAdapter
+    private lateinit var category: ArrayList<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,14 +35,17 @@ class MainActivity : AppCompatActivity() {
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
 
+        // init article rv
+        setUpArticleRV()
+
         // init show viewModel
         val repo = Repo(ArticleDatabase(this))
         val viewModelProviderFactory = NewsViewModelProviderFactory(repo)
         viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
 
 
-        // add category
-        val category = ArrayList<Category>()
+        // add category list
+        category = ArrayList()
         category.add(Category("Business", Utils.NY_BUSINESS))
         category.add(Category("Education", Utils.NY_EDUCATION))
         category.add(Category("Science", Utils.NY_SCIENCE))
@@ -53,80 +54,48 @@ class MainActivity : AppCompatActivity() {
         category.add(Category("Tech", Utils.NY_TECH))
         category.add(Category("Your money", Utils.NY_YOURMONEY))
 
+
         // attach category list to adapter
-        val adapter = CategoryAdapter(category)
+        categoryAdapter = CategoryAdapter(category)
         category_rv.rootView.post {
-            category_rv.adapter = adapter
-            category_rv.layoutManager =
-                LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+            category_rv.adapter = categoryAdapter
+            category_rv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         }
 
-        // onclick select source & observe live data change
-        adapter.setOnItemClickListener {
-            progress_view.visibility = View.VISIBLE
-            sourceObserver(it.source)
+        // observe changes on topic change for list
+        viewModel.currentTopic.observe(this, Observer {
+            startCrawlObserver(it.toString())
+        })
+
+
+        // onclick to select source & post value to liveData
+        categoryAdapter.setOnItemClickListener {
+            viewModel.currentTopic.value = it.source
+        }
+
+
+        // pass data onclick
+        newsAdapter.setOnItemClickListener { articleLink ->
+            val intent = Intent(this@MainActivity, NewsDetailsActivity::class.java)
+            intent.putExtra("title", articleLink.title)
+            intent.putExtra("description", articleLink.description)
+            intent.putExtra("image", articleLink.image)
+            intent.putExtra("author", articleLink.author)
+            intent.putExtra("source", articleLink.source)
+            startActivity(intent)
         }
 
 
     }
 
-    private fun sourceObserver(url: String?) {
-
-
-        GlobalScope.launch(Dispatchers.Main) {
-            url?.let { url ->
-                viewModel.getTopic(url).observe(this@MainActivity, Observer { topic ->
-                    val document = Jsoup.connect(topic).get()
-                    val articles = mutableListOf<Article>()
-
-                    // Path of articles in web
-                    val articleHTML = document.getElementById("stream-panel")
-                        .select("div").select("ol")
-                        .select("div").select("div").select("a")
-
-                    articleHTML.forEach { item ->
-                        // iterate each article to get content
-                        val image = item.select("div").select("figure")
-                            .select("div").select("img").attr("src")
-                        val title = item.select("h2").text()
-                        val description = item.select("p").text()
-                        val author = item.select("div").select("p").select("span").text()
-                        val source = item.attr("href")
-
-                        if (!image.isNullOrEmpty() || !title.isNullOrEmpty() || !description.isNullOrEmpty() || !author.isNullOrEmpty() || !source.isNullOrEmpty()) {
-                            val article = Article(title, description, image, author, source)
-                            articles.add(article)
-                        }
-                    }
-
-                    // init adapter
-                    val adapter = NewsAdapter()
-                    article_rv.rootView.post {
-                        article_rv.adapter = adapter
-                        article_rv.layoutManager = LinearLayoutManager(this@MainActivity)
-                        article_rv.visibility = View.VISIBLE
-                        progress_view.visibility = View.GONE
-                    }
-
-                    // submit articles list to adapter
-                    adapter.differ.currentList.clear()
-                    adapter.differ.submitList(articles)
-
-                    // onclick open news details activity
-                    adapter.setOnItemClickListener { articleLink ->
-                        val intent = Intent(this@MainActivity, NewsDetailsActivity::class.java)
-                        intent.putExtra("title", articleLink.title)
-                        intent.putExtra("description", articleLink.description)
-                        intent.putExtra("image", articleLink.image)
-                        intent.putExtra("author", articleLink.author)
-                        intent.putExtra("source", articleLink.source)
-                        startActivity(intent)
-                    }
-                })
-            }
-
+    private fun startCrawlObserver(url: String?) {
+        progress_view.visibility = View.VISIBLE
+        url?.let { currentTopic ->
+            viewModel.crawlFromNY(currentTopic).observe(this@MainActivity, Observer { list ->
+                newsAdapter.differ.submitList(list)
+                progress_view.visibility = View.GONE
+            })
         }
-
     }
 
 
@@ -144,9 +113,17 @@ class MainActivity : AppCompatActivity() {
                 startActivity(Intent(this, BookmarkActivity::class.java))
                 true
             }
-
             else -> super.onOptionsItemSelected(item)
         }
+    }
 
+    private fun setUpArticleRV() {
+        newsAdapter = NewsAdapter()
+        article_rv.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+            article_rv.visibility = View.VISIBLE
+
+        }
     }
 }
--- app/src/main/java/www/thecodemonks/techbytes/viewmodel/NewsViewModel.kt
+++ app/src/main/java/www/thecodemonks/techbytes/viewmodel/NewsViewModel.kt
@@ -3,35 +3,68 @@ package www.thecodemonks.techbytes.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.jsoup.Jsoup
 import www.thecodemonks.techbytes.model.Article
 import www.thecodemonks.techbytes.repo.Repo
+import www.thecodemonks.techbytes.utils.Utils
 
 class NewsViewModel(private val repo: Repo) : ViewModel() {
-    // selected source
-    fun getTopic(source: String): MutableLiveData<String> {
-        selectedSource.postValue(source)
-        return selectedSource
-    }
 
+    var crawledFromNY: MutableLiveData<List<Article>> = MutableLiveData()
+    val currentTopic: MutableLiveData<String> by lazy {
+        MutableLiveData<String>().defaultTopic(Utils.NY_TECH)
+    }
 
     // save article
     fun upsertArticle(article: Article) = viewModelScope.launch {
         repo.upsertArticle(article)
     }
 
-
     // get saved article
     fun getSavedArticle() = repo.getSavedArticle()
-
 
     // save article
     fun deleteArticle(article: Article) = viewModelScope.launch {
         repo.deleteArticle(article)
     }
 
+    fun crawlFromNY(url: String): MutableLiveData<List<Article>> {
 
-    var selectedSource: MutableLiveData<String> = MutableLiveData()
+        viewModelScope.launch(Dispatchers.Main) {
+            val document = Jsoup.connect(url).get()
+            val articles: MutableList<Article> = mutableListOf()
 
+            // Path of articles present in the web
+            val articleHTML = document.getElementById("stream-panel")
+                    .select("div").select("ol")
+                    .select("div").select("div").select("a")
+
+            // iterate each article to get content
+            articleHTML.forEach { item ->
+                val image = item.select("div").select("figure")
+                        .select("div").select("img").attr("src")
+                val title = item.select("h2").text()
+                val description = item.select("p").text()
+                val author = item.select("div").select("p").select("span").text()
+                val source = item.attr("href")
+
+                // check for null content
+                if (!image.isNullOrEmpty() || !title.isNullOrEmpty() || !description.isNullOrEmpty() || !author.isNullOrEmpty() || !source.isNullOrEmpty()) {
+                    val article = Article(title, description, image, author, source)
+                    articles.add(article)
+                }
+            }
+
+            // post value to after crawling to liveData
+            crawledFromNY.value = articles
+        }
+
+        return crawledFromNY
+    }
+
+
+    private fun <T : Any?> MutableLiveData<T>.defaultTopic(initialValue: T) = apply { setValue(initialValue) }
 
 }
