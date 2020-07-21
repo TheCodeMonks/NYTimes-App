ba7327992871fdc8e6c613dd12c5e340328db892
Sanju S <spikeysanju98@gmail.com>
2020-07-21 12:52:46 +0530

    📸 Updated screenshots & art
    Signed-off-by: Sanju S <spikeysanju98@gmail.com>

--- app/src/main/java/www/thecodemonks/techbytes/NYTimes.kt
+++ app/src/main/java/www/thecodemonks/techbytes/NYTimes.kt
@@ -39,7 +39,7 @@ class NYTimes : Application() {
         val mode = if (isNight()) {
             AppCompatDelegate.MODE_NIGHT_YES
         } else {
-            AppCompatDelegate.MODE_NIGHT_YES
+            AppCompatDelegate.MODE_NIGHT_NO
         }
 
         AppCompatDelegate.setDefaultNightMode(mode)
