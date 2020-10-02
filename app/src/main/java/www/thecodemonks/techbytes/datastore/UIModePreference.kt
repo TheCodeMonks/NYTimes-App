b4b478991b7ae8409aebfb105dac1bb498d10030
sourabh <sourabhsnath@gmail.com>
2020-10-02 11:57:17 +0530

    Added datastore to save Day/Night Mode preference

--- app/src/main/java/www/thecodemonks/techbytes/datastore/UIModePreference.kt
+++ app/src/main/java/www/thecodemonks/techbytes/datastore/UIModePreference.kt
@@ -30,11 +30,30 @@ import android.content.Context
 import androidx.datastore.DataStore
 import androidx.datastore.preferences.Preferences
 import androidx.datastore.preferences.createDataStore
+import androidx.datastore.preferences.edit
+import androidx.datastore.preferences.preferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class UIModePreference (context: Context) {
+class UIModePreference(context: Context) {
+
+    companion object {
+        private val UI_MODE_KEY = preferencesKey<Boolean>("ui_mode")
+    }
 
     private val dataStore: DataStore<Preferences> = context.createDataStore(
-        name = "ui_mode"
+        name = "ui_mode_preference"
     )
 
+    suspend fun saveToDataStore(isNightMode: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[UI_MODE_KEY] = isNightMode
+        }
+    }
+
+    val uiMode: Flow<Boolean> = dataStore.data
+        .map { preferences ->
+            val uiMode = preferences[UI_MODE_KEY] ?: false
+            uiMode
+        }
 }
