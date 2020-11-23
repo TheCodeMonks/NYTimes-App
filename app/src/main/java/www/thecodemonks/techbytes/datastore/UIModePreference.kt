14050633bbe2e47d39f444af022bc9ac2b6c6e5f
Sanju S <spikeysanju98@gmail.com>
2020-11-23 21:54:02 +0530

    Refactor Code
    Signed-off-by: Sanju S <spikeysanju98@gmail.com>

--- app/src/main/java/www/thecodemonks/techbytes/datastore/UIModePreference.kt
+++ app/src/main/java/www/thecodemonks/techbytes/datastore/UIModePreference.kt
@@ -27,11 +27,11 @@
 package www.thecodemonks.techbytes.datastore
 
 import android.content.Context
-import androidx.datastore.DataStore
-import androidx.datastore.preferences.Preferences
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.preferencesKey
 import androidx.datastore.preferences.createDataStore
-import androidx.datastore.preferences.edit
-import androidx.datastore.preferences.preferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
